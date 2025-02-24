e8d1d9fcefdec17f87abfc68b1b16752178d34b3
Garret Reichenbach <garretreichenbach@gmail.com>
2025-02-24 12:23:02 -0700

    Update JavaVersion.java

--- Starter/src/main/java/smlauncher/downloader/JavaVersion.java
+++ Starter/src/main/java/smlauncher/downloader/JavaVersion.java
@@ -9,7 +9,7 @@ public enum JavaVersion {
 
 //	JAVA_8(8, "jdk8", "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u392-b08/OpenJDK8U-jre_x64_%s_hotspot_8u392b08.%s"),
 //	JAVA_18(18, "jdk-18", "https://github.com/adoptium/temurin18-binaries/releases/download/jdk-18.0.2.1%%2B1/OpenJDK18U-jre_x64_%s_hotspot_18.0.2.1_1.%s");
-	JAVA_21(21, "jdk-21", "https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5.1%%2B11/OpenJDK21U-jre_x64_%s_hotspot_21.0.5_11.%s");
+	JAVA_21(21, "jdk-21", "https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%%2B11/OpenJDK21U-jre_x64_%s_hotspot_21.0.5_11.%s");
 	
 	public final int number; // Version number
 	public final String fileStart; // JDK folder header
