aef34348b47c52b60880f03239594db57403d77c
Garret Reichenbach <garretreichenbach@gmail.com>
2025-04-25 15:59:04 -0700

    fixed java stuff

--- src/main/java/smlauncher/StarMadeLauncher.java
+++ src/main/java/smlauncher/StarMadeLauncher.java
@@ -409,9 +409,9 @@ public class StarMadeLauncher extends JFrame {
 
 	private static String getJavaPath() {
 		if(gameVersion.version.startsWith("0.2") || gameVersion.version.startsWith("0.1")) {
-			return LaunchSettings.getInstallDir() + "/" + String.format(currentOS.javaPath, 8);
+			return String.format(currentOS.javaPath, 8);
 		} else {
-			return LaunchSettings.getInstallDir() + "/" + String.format(currentOS.javaPath, 23);
+			return String.format(currentOS.javaPath, 23);
 		}
 	}
 
